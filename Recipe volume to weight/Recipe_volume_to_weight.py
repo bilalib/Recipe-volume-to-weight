9506e08681c8a5aab53ac26cf6da4d403ff03c30
Bilal Ibrahim <bilalib@umich.edu>
2019-06-02 20:14:12 -0400

    Bug fixes with file locations

--- Recipe volume to weight/Recipe_volume_to_weight.py
+++ Recipe volume to weight/Recipe_volume_to_weight.py
@@ -1,4 +1,5 @@
 from Recipe import *
+import os
 
 link = "https://www.allrecipes.com/recipe/236165/fluffy-and-delicious-pancakes/?internalSource=previously%20viewed&referringContentType=Homepage&clickId=cardslot%206"
 recipe = Recipe.from_link(link)
